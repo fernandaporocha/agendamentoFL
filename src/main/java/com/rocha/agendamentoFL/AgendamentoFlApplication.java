0de0cc4f28aa70cf1ff3db0dce4b8914f17634db
Fernanda Rocha <fernandaporocha@gmail.com>
2020-02-29 18:57:00 +0000

    Configuracao inicial do projeto

---
+++ src/main/java/com/rocha/agendamentoFL/AgendamentoFlApplication.java
@@ -0,0 +1,13 @@
+package com.rocha.agendamentoFL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoFlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoFlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocha/agendamentoFL/AgendamentoFlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocha.agendamentoFL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoFlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
